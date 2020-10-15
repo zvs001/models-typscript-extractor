69a57d95074e136bb6aee6fa31c08993428f87ca
Vlad Zaynchkovsly (mac) <zvsx001@gmail.com>
2020-10-15 18:59:55 +0300

    some progress

---
+++ extractTypings.ts
@@ -0,0 +1,91 @@
+import path from 'path'
+import _ from 'lodash'
+import * as ts from 'typescript'
+
+function extractTypings(file: string, config: any): string | null {
+  // Create a Program to represent the project, then pull out the
+  // source file to parse its AST.
+  const program = ts.createProgram([file], { allowJs: true })
+  const sourceFile = program.getSourceFile(file)
+  if (!sourceFile) return null
+
+  // To print the AST, we'll use TypeScript's printer
+  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
+
+  // To give constructive error messages, keep track of found and un-found identifiers
+  const resultNodes = filterNodes(sourceFile, config)
+  if (_.isEmpty(resultNodes)) return null
+
+  const resultFile = ts.createSourceFile('./tttt11.ts', '', ts.ScriptTarget.Latest, /* setParentNodes */ false, ts.ScriptKind.TS)
+
+  const output = resultNodes.map(node => printer.printNode(ts.EmitHint.Unspecified, node, resultFile)).join('\n').trim()
+
+  return output
+}
+
+function filterNodes(sourceFile: ts.SourceFile, { rootDir, interfaces }: any): ts.Node[] {
+  const resultNodes: ts.Node[] = []
+  ts.forEachChild(sourceFile, node => {
+    let name = ''
+
+    // This is an incomplete set of AST nodes which could have a top level identifier
+    // it's left to you to expand this list, which you can do by using
+    // https://ts-ast-viewer.com/ to see the AST of a file then use the same patterns
+    // as below
+    if (ts.isFunctionDeclaration(node)) {
+      return null
+      console.log('isFunctionDeclaration')
+      name = node.name.text
+      // Hide the method body when printing
+      node.body = undefined
+    } if (ts.isVariableStatement(node)) {
+      return null
+
+      console.log('isVariableStatement')
+
+      name = node.declarationList.declarations[0].name.getText(sourceFile)
+    } if (ts.isInterfaceDeclaration(node)) {
+      name = node.name.text
+      const { ignore } = interfaces || {}
+      let isIgnored = false
+      _.forEach(ignore, regex => {
+        if (name.match(regex)) isIgnored = true
+      })
+
+      if (isIgnored) return null
+      console.log('isInterfaceDeclaration')
+    } else if (ts.isTypeAliasDeclaration(node)) {
+      console.log('isTypeParameterDeclaration')
+      name = node.name.text
+    } else if (ts.isImportDeclaration(node)) {
+      console.log('isImportDeclaration')
+
+      name = node.moduleSpecifier.text
+      if (name.includes('..')) {
+        const rootDirPath = path.resolve(rootDir)
+        const importPath = name
+        const filePath = sourceFile.path
+        const fileDir = path.dirname(filePath)
+        const libFullPath = path.resolve(fileDir, importPath)
+        if (!libFullPath.includes(rootDir)) {
+          return null
+        }
+      } else {
+        // module import
+        return null
+      }
+    } else if (ts.isExportAssignment(node)) {
+      return null
+    } else if (ts.isExportSpecifier(node)) {
+      // console.log('isExportSpecifier')
+    }
+
+    console.log('name', name)
+    if (!node || !name) return null
+    resultNodes.push(node)
+  })
+
+  return resultNodes
+}
+
+export default extractTypings
---
+++ index.ts
@@ -0,0 +1,58 @@
+import fs from 'fs'
+import * as path from 'path'
+import glob from 'glob'
+import minimatch from 'minimatch'
+import extractTypings from './extractTypings'
+
+const config = {
+  rootDir: './models',
+  outputDir: './typings/core',
+  match: './models/*',
+  interfaces: {
+    ignore: [
+      /Payload$/,
+    ],
+  },
+}
+
+// const a = minimatch('.', config.match)
+glob(config.match, (err, files) => {
+  if (err) {
+    console.error(err)
+    return null
+  }
+  console.log('files', files)
+
+  const fileResultPath = path.join(config.outputDir, 'index.d.ts')
+  fs.mkdirSync(config.outputDir, { recursive: true })
+  fs.writeFileSync(fileResultPath, '', { encoding: 'utf8' })
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i]
+    console.log(`[${i + 1}/${files.length}]`, 'process', file)
+    processFile(file, {
+      fileResultPath,
+    })
+  }
+})
+// console.log('a', a)
+
+function processFile(filePath: string, { fileResultPath }: { fileResultPath: string}) {
+  const resultTypings = extractTypings(filePath, config)
+  console.log('--- ts --- ')
+  console.log(resultTypings)
+  if (!resultTypings) {
+    console.error('resultTypings is null. Skip')
+    return null
+  }
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
+  const relativeFilePath = filePath.replace(config.rootDir, '')
+  let newFilePath = path.join(config.outputDir, relativeFilePath)
+  newFilePath = newFilePath.replace(/.ts$/, '.d.ts')
+  console.log('newFilePath', newFilePath)
+  const typingsTitle = `/**********   ${relativeFilePath}    **********/ \n`
+  fs.appendFileSync(fileResultPath, typingsTitle + resultTypings, { encoding: 'utf8' })
+}
